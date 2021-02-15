f953a704ff0e290a03e9e82fed3e1618c13aee5f
Lynn-Apedo <lynnapedo@gmail.com>
2021-02-15 11:35:56 +0100

    conditional nav, logout

--- front/src/components/molecules/Nav.jsx
+++ front/src/components/molecules/Nav.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import ContextAuth from "../authentification/ContextAuth";
 
 // import { Link } from "react-router-dom";
 
 export default function Nav() {
+    const { state } = useContext(ContextAuth);
+
+    const Logout = () => {
+        localStorage.clear();
+        window.location.href = "/"; // ????
+    }
+
+    useEffect(() => {
+        return () => {}
+    }, [state.isAuthenticated]);
+
+    if (state.isAuthenticated === true) {
+        return (
+            <nav>
+                <ul className="nav-links">
+                    <Link to="/logout">
+                        <li onClick={Logout}>Déconnexion</li>
+                    </Link>
+                </ul>
+            </nav>
+        )
+    }
     return (
         <nav>
             <ul className="nav-links">
-                {/* <Link to="/"> */}
-                    <li>Déconnexion</li>
-                {/* </Link> */}
+                {/* <Link to="/logout">
+                    <li onClick={Logout}>Déconnexion</li>
+                </Link> */}
             </ul>
         </nav>
     )
+    
 }
